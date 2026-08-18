54f5f68961b1793927be81122f9c83f816cde853
Azmira Tania <azmiratania@users.noreply.github.com>
2026-08-18 12:35:32 +0800

    Create Player class with name and color properties

---
+++ Player.ts
@@ -0,0 +1,9 @@
+class Player {
+    public readonly name: string;
+    public readonly color: DiscColor;
+
+    constructor(name: string, color: DiscColor) {
+        this.name = name;
+        this.color = color;
+    }
+}
