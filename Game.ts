7bcf86b18ba642d71cb1150eb933b510d77d016e
Azmira Tania <azmiratania@users.noreply.github.com>
2026-08-18 12:19:40 +0800

    Implement Game class with game logic

---
+++ Game.ts
@@ -0,0 +1,64 @@
+enum GameState {
+    IN_PROGRESS = "IN_PROGRESS",
+    WON = "WON",
+    DRAW = "DRAW",
+}
+
+class Game {
+    private readonly board: Board;
+    private readonly player1: Player;
+    private readonly player2: Player;
+    private currentPlayer: Player;
+    private state: GameState;
+    private winner: Player | null;
+
+    constructor(player1: Player, player2: Player) {
+        this.board = new Board();
+        this.player1 = player1;
+        this.player2 = player2;
+        this.currentPlayer = player1;
+        this.state = GameState.IN_PROGRESS;
+        this.winner = null;
+    }
+
+    makeMove(player: Player, column: number): boolean {
+        if (this.state !== GameState.IN_PROGRESS) {
+            return false;
+        }
+        if (player !== this.currentPlayer) {
+            return false;
+        }
+
+        const row = this.board.placeDisc(column, player.color);
+        if (row === -1) {
+            return false;
+        }
+
+        if (this.board.checkWin(row, column, player.color)) {
+            this.state = GameState.WON;
+            this.winner = player;
+        } else if (this.board.isFull()) {
+            this.state = GameState.DRAW;
+        } else {
+            this.currentPlayer = this.currentPlayer === this.player1 ? this.player2 : this.player1;
+        }
+
+        return true;
+    }
+
+    getCurrentPlayer(): Player {
+        return this.currentPlayer;
+    }
+
+    getGameState(): GameState {
+        return this.state;
+    }
+
+    getWinner(): Player | null {
+        return this.winner;
+    }
+
+    getBoard(): Board {
+        return this.board;
+    }
+}
